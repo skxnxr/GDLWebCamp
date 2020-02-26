a857f0c368044d74ce92b5622a12c1d9ea38a9b8
Skyner <sacwks@gmailcom>
2020-02-26 18:21:26 -0400

    Introduciendo cambios video 199

---
+++ js/main.js
@@ -0,0 +1,25 @@
+(function() {
+    "use strict";
+
+    document.addEventListener('DOMContentLoaded', function(){
+
+    //Datos usuarios
+    let nombre = document.getElementById('nombre');
+    let apellido = document.getElementById('apellido');
+    let email = document.getElementById('email');
+
+    //Campos pases 
+    let pase_dia = document.getElementById('pase_dia');
+    let pase_dosdias = document.getElementById('pase_dosdias');
+    let pase_completo = document.getElementById('pase_completo');
+    
+    //Botonoes y divs
+    let calcular = document.getElementById('calcular');
+    let errorDiv = document.getElementById('error');
+    let botonRegistro = document.getElementById('btnRegistro');
+    let resultado = document.getElementById('lista-productos');
+
+
+
+    }); //DOM CONTENT LOADED
+})();
